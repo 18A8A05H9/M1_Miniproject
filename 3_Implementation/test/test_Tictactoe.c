d241a98d71cf05c28e11ce5d12221b5017b5fd05
18A8A05H9 <101167980+18A8A05H9@users.noreply.github.com>
2022-04-01 18:28:25 +0530

    Create test_Tictactoe.c

---
+++ 3_Implementation/test/test_Tictactoe.c
@@ -0,0 +1,39 @@
+#include "unity.h"
+
+#include "methodfunction.h"
+#include "user.h"
+
+
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+
+
+/* Prototypes for all the test functions */
+
+
+void test_unity(void){
+        
+        int sum=1000;
+  	TEST_ASSERT_EQUAL(1,fun6(sum));
+        TEST_ASSERT_EQUAL(NULL,main());
+        
+      
+}
+
+
+
+
+int main(void)
+{
+UNITY_BEGIN();
+RUN_TEST(test_unity);
+
+return UNITY_END();
+}
